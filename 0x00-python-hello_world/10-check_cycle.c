b4a29e7d1dea76d7da9e05855f855ed1f14890f3
FoleKhali <dhannaps03@gmail.com>
2022-04-25 20:31:06 -0700

    check cycles

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * check_cycle - Checks if a single list has a cycle in it
+ * @list: checker
+ * Return: 0 if there is not a cycle and 1 if it is
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *_single = list;
+	listint_t *_double = list;
+
+	if (list == NULL)
+		return (0);
+
+	while (_single != NULL && _double != NULL)
+	{
+		_single = _single->next;
+		_double = _double->next->next;
+		if (_double == _single)
+			return (1);
+	}
+
+	return (0);
+}
