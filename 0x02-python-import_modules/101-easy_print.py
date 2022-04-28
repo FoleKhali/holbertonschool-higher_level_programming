fe492134cf33a93a1c022351c87f239ae30fa1ad
FoleKhali <platanobrgas@gmail.com>
2022-04-28 18:44:25 -0500

    print without print

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import os
-s = "#pythoniscool\n"
-line = str.encode(s)
-os.write(1, line)
+if __name__ == "__main__":
+    s = "#pythoniscool\n"
+    line = str.encode(s)
+    os.write(1, line)
