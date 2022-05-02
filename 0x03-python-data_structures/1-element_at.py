eced29024e37ee5ba54d97c703fd4991b158a0db
FoleKhali <platanobrgas@gmail.com>
2022-05-02 16:27:06 -0500

    Function that retrieves an element of a list like c

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    a = len(my_list) - 1
     for i in my_list:
         if idx < 0:
             return None
-        elif idx > a:
+        elif(idx > len(my_list) - 1):
             return None
         else:
-            return a
+            return (my_list[idx])
