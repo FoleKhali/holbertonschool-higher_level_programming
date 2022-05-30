8ad7ab88cff63c92eb5519b744e23ebee8e5dd3e
FoleKhali <platanobrgas@gmail.com>
2022-05-30 18:40:15 -0500

    fixes

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""function that returns attributes"""
 def lookup(obj):
     """return a lits of the availables attributes and methonds"""
     return dir(obj)
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""return true if its exactly an intance, otherwise, return false"""
 def is_same_class(obj, a_class):
     """check if object is an instance of a_class"""
     if type(obj) is a_class:
