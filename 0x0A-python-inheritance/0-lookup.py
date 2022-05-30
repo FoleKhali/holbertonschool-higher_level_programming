ca4df0e1fdf31eb6bc08537f698898da84eef7ed
FoleKhali <platanobrgas@gmail.com>
2022-05-30 18:13:44 -0500

    Lookup and Mylist

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """return a lits of the availables attributes and methonds"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,5 @@
+def is_same_class(obj, a_class):
+    """check if object is an instance of a_class"""
+    if type(obj) is a_class:
+        return True
+    return False
