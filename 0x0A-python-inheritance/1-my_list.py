a95a5ee56e1b3d4ca01473cbba2fb8a685a7b5b3
FoleKhali <platanobrgas@gmail.com>
2022-05-30 21:36:54 -0500

    dl

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""a sorted list"""
+
+
 class MyList(list):
     """creates a subclass Mylist of superclass list"""
     def print_sorted(self):
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""empty class Geometry"""
+
+
+class BaseGeometry:
+    """class """
+
+    def area(self):
+        """area"""
+        raise Exception("area() is not implemented")
