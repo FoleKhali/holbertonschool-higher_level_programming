7d04f1f8b825062bb0c3dd4eaff7eadc30b7c4f1
FoleKhali <dhannaps03@gmail.com>
2022-07-07 00:53:16 -0500

    adding 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,6 @@
--- Script that creates the database hbtn_0d_2 and the user user_0d_2.
-CREATE DATABASE
-       IF NOT EXISTS hbtn_0d_2;
-CREATE USER
-       IF NOT EXISTS 'user_0d_2'@'localhost'
-       IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-      ON `hbtn_0d_2`.*
-      TO 'user_0d_2'@'localhost'
-      IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+-- Script that create a new database if it doesn't exits
+-- Script that create a new users if it doesn't exits
+-- Script to give only SELECT permission
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
