2c28f59f5951b753b0688ff2d455d074e98836fe
FoleKhali <dhannaps03@gmail.com>
2022-09-13 22:47:03 -0500

    adding 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 /* module request */
 const request = require('request');
 
-const page = 'http://swapi.co/api/films/' + process.argv[2];
+const page = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(page, function (error, status, body) {
   if (error) {
     console.error(error);
